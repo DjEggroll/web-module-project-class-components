761a4f6101f318f9eeb4aef93151b60cee2663f7
Jiahui Feng <feng_jeffrey@yahoo.com>
2022-06-06 16:33:36 -0400

    Data set up and passed to list

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,10 +1,33 @@
 import React from 'react'
+import TodoList from 'TodoList.js';
+
+const toDoList = [
+  {
+    name: 'Organize Garage',
+    id: 1528817077286, // could look different, you could use a timestamp to generate it
+    completed: false
+  },
+  {
+    name: 'Bake Cookies',
+    id: 1528817084358,
+    completed: false
+  }
+];
 
 export default class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      toDoList: toDoList
+    }
+  }
+
   render() {
     return (
       <div>
         Todo App
+
+        <TodoList toDoList={this.state.toDoList} />
       </div>
     )
   }
